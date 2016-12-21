92651f4c45a7fc1cde0a09182d51e5779791edf6
Rosanne Hoyem <rosiehoyem@Rosannes-MacBook-Air.local>
2016-12-21 09:51:37 -0600

    completed advanced_python_csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,29 @@
-PLACE YOUR CODE HERE
+import sys
+import csv
+
+
+def list_of_emails(csv_file):
+	with open(csv_file) as csvfile:
+		reader = csv.DictReader(csvfile, skipinitialspace=True)
+		emails = []
+		for row in reader:
+			emails.append(row['email'])
+	
+		fieldnames = ['email']
+		writer = csv.DictWriter(open('emails.csv', 'w'), fieldnames=fieldnames)
+		writer.writeheader()
+		for email in emails:
+			writer.writerow({'email': email})
+	return writer
+
+def main():
+    arg = sys.argv[1]
+
+    if not arg:
+        print('Error: missing csv argument')
+        sys.exit(1)
+
+    list_of_emails(arg)
+  
+if __name__ == '__main__':
+  main()
