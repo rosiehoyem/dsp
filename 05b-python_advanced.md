b27f1f821a7f89258a4eb8d3e690241539d3ce57
Rosie Hoyem <rosiehoyem@gmail.com>
2017-01-02 19:18:36 -0600

    completed advanced_python_dict

--- 05b-python_advanced.md
+++ 05b-python_advanced.md
@@ -16,25 +16,30 @@ This data is available in this file:  [faculty.csv](python/faculty.csv)
 
 ###Part I - Regular Expressions  
 
-
 ####Q1. Find how many different degrees there are, and their frequencies: Ex:  PhD, ScD, MD, MPH, BSEd, MS, JD, etc.
 
->> REPLACE THIS WITH YOUR RESPONSE
+The number of different degrees is 8.
+The frequencies of these degrees are:
+{'MPH': 2, 'MS': 2, 'JD': 1, 'ScD': 6, 'MA': 1, 'MD': 1, 'BSEd': 1, 'PhD': 31}
 
 
 ####Q2. Find how many different titles there are, and their frequencies:  Ex:  Assistant Professor, Professor
 
->> REPLACE THIS WITH YOUR RESPONSE
+The number of different titles is 3.
+The frequencies of these titles are:
+{'Associate Professor of Biostatistics': 12, 'Professor of Biostatistics': 13, 'Assistant Professor of Biostatistics': 12}
 
 
 ####Q3. Search for email addresses and put them in a list.  Print the list of email addresses.
 
->> REPLACE THIS WITH YOUR RESPONSE
+['bellamys@mail.med.upenn.edu', 'warren@upenn.edu', 'bryanma@upenn.edu', 'jinboche@upenn.edu', 'sellenbe@upenn.edu', 'jellenbe@mail.med.upenn.edu', 'ruifeng@upenn.edu', 'bcfrench@mail.med.upenn.edu', 'pgimotty@upenn.edu', 'wguo@mail.med.upenn.edu', 'hsu9@mail.med.upenn.edu', 'rhubb@mail.med.upenn.edu', 'whwang@mail.med.upenn.edu', 'mjoffe@mail.med.upenn.edu', 'jrlandis@mail.med.upenn.edu', 'liy3@email.chop.edu', 'mingyao@mail.med.upenn.edu', 'hongzhe@upenn.edu', 'rlocalio@upenn.edu', 'nanditam@mail.med.upenn.edu', 'knashawn@mail.med.upenn.edu', 'propert@mail.med.upenn.edu', 'mputt@mail.med.upenn.edu', 'sratclif@upenn.edu', 'michross@upenn.edu', 'jaroy@mail.med.upenn.edu', 'msammel@cceb.med.upenn.edu', 'shawp@upenn.edu', 'rshi@mail.med.upenn.edu', 'hshou@mail.med.upenn.edu', 'jshults@mail.med.upenn.edu', 'alisaste@mail.med.upenn.edu', 'atroxel@mail.med.upenn.edu', 'rxiao@mail.med.upenn.edu', 'sxie@mail.med.upenn.edu', 'dxie@upenn.edu', 'weiyang@mail.med.upenn.edu']
 
 
 ####Q4. Find how many different email domains there are (Ex:  mail.med.upenn.edu, upenn.edu, email.chop.edu, etc.).  Print the list of unique email domains.
 
->> REPLACE THIS WITH YOUR RESPONSE
+The number of different email domains is 4.
+The frequencies of these domains are:
+{'mail.med.upenn.edu': 23, 'cceb.med.upenn.edu': 1, 'upenn.edu': 12, 'email.chop.edu': 1}
 
 Place your code in this file: [advanced_python_regex.py](python/advanced_python_regex.py)
 
--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,96 @@
-PLACE YOUR CODE HERE
+import sys
+import csv
+import operator
+import itertools as it
+
+### Part III - Dictionary
+
+####Q6.  Create a dictionary in the below format:
+'''
+faculty_dict = { 'Ellenberg': [['Ph.D.', 'Professor', 'sellenbe@upenn.edu'], ['Ph.D.', 'Professor', 'jellenbe@mail.med.upenn.edu']],
+              'Li': [['Ph.D.', 'Assistant Professor', 'liy3@email.chop.edu'], ['Ph.D.', 'Associate Professor', 'mingyao@mail.med.upenn.edu'], ['Ph.D.', 'Professor', 'hongzhe@upenn.edu']]}
+
+Print the first 3 key and value pairs of the dictionary:
+'''
+
+def take(n, iterable):
+    #Return first n items of the iterable as a list
+    return list(it.islice(iterable, n))
+
+def dict_sample(dictionary):
+	sample = take(3, dictionary)
+	dictionary_sample = {}
+	for item in sample:
+		dictionary_sample[item] = dictionary[item]
+	return dictionary_sample
+
+def create_lastname_dict(csv_file):
+	with open(csv_file) as csvfile:
+		reader = csv.DictReader(csvfile, skipinitialspace=True)
+		fdict = {}
+		for row in reader:
+			last_name = row['name'].split()[-1]
+			if last_name in fdict:
+				fdict[last_name].append([row['degree'], row['title'], row['email']])
+			else:
+				fdict[last_name] = []
+				fdict[last_name].append([row['degree'], row['title'], row['email']])
+	return dict_sample(fdict)
+
+####Q7.  The previous dictionary does not have the best design for keys.  Create a new dictionary with keys as:
+
+'''
+professor_dict = {('Susan', 'Ellenberg'): ['Ph.D.', 'Professor', 'sellenbe@upenn.edu'], ('Jonas', 'Ellenberg'): ['Ph.D.', 'Professor', 'jellenbe@mail.med.upenn.edu'], ('Yimei', 'Li'): ['Ph.D.', 'Assistant Professor', 'liy3@email.chop.edu'], ('Mingyao','Li'): ['Ph.D.', 'Associate Professor', 'mingyao@mail.med.upenn.edu'], ('Hongzhe','Li'): ['Ph.D.', 'Professor', 'hongzhe@upenn.edu'] }
+
+Print the first 3 key and value pairs of the dictionary:
+'''
+
+def create_fullname_dict(csv_file):
+	with open(csv_file) as csvfile:
+		reader = csv.DictReader(csvfile, skipinitialspace=True)
+		fdict = {}
+		for row in reader:
+			first_name = row['name'].split()[0]
+			last_name = row['name'].split()[-1]
+			name_key = (last_name, first_name)
+			fdict[name_key] = []
+			fdict[name_key].append(row['degree'])
+			fdict[name_key].append(row['title'])
+			fdict[name_key].append(row['email'])
+	return dict_sample(fdict)
+
+'''
+####Q8.  It looks like the current dictionary is printing by first name.  Print out the dictionary key value 
+pairs based on alphabetical orders of the last name of the professors
+'''
+
+def create_sorted_fullname_dict(csv_file):
+	with open(csv_file) as csvfile:
+		reader = csv.DictReader(csvfile, skipinitialspace=True)
+		fdict = {}
+		for row in reader:
+			first_name = row['name'].split()[0]
+			last_name = row['name'].split()[-1]
+			name_key = (last_name, first_name)
+			fdict[name_key] = []
+			fdict[name_key].append(row['degree'])
+			fdict[name_key].append(row['title'])
+			fdict[name_key].append(row['email'])
+	for item in sorted(fdict, key=operator.itemgetter(0)):
+		print(item)
+		
+
+def main():
+	arg = sys.argv[1]
+	if not arg:
+		print('ERROR: include csv file as argument')
+		sys.exit(1)
+	
+	print(create_lastname_dict(arg))
+	print("---------------")
+	print(create_fullname_dict(arg))
+	print("---------------")
+	create_sorted_fullname_dict(arg)
+  
+if __name__ == '__main__':
+  main()
